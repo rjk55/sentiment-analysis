c284827a11898d846d50285cebe0d1857f358625
rjk55 <rahuljayaraj55@gmail.com>
2023-01-09 10:32:42 +0530

    return json response

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.views import View
 from scrapper.base import Scrapper, get_scrapper
 from analysis.main import analyze_polarity
 import logging
+import json
+from django.http import JsonResponse
 
 logger = logging.getLogger(__name__)
 
@@ -14,7 +16,11 @@ class EnterProductURLView(View):
         return render(request, 'index.html', )
 
     def post(self, request):
-        url = request.POST["url"]
+        body_unicode = request.body.decode('utf-8')
+        received_json = json.loads(body_unicode)
+
+        url = received_json["url"]
+
         logger.info("Received URL: %s", url)
         
         scrapper_class = get_scrapper(url)
@@ -25,4 +31,4 @@ class EnterProductURLView(View):
         # convert tuple to list
         sentiment = list(analyze_polarity(reviews))
         logger.info("Sentiment: %s", sentiment)
-        return render(request, 'results.html', {"sentiment": sentiment, "product_detail": product_detail})
+        return JsonResponse({"sentiment": sentiment, "product_detail": product_detail} ,safe=False)
